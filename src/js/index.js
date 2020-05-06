0a7a5627799883ba37f29814b2111dc655453956
ridwan57 <ridwananik57@gmail.com>
2020-05-07 04:53:41 +0600

    9.17

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,29 @@
 // Global app controller
-import x from './test';
-console.log(x);
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
+
+const state = {};
+
+const controlsearch = async () => {
+    const query = searchView.getInput();
+    console.log(query);
+    if (query) {
+        state.search = new Search(query);
+        searchView.clearInput();
+        searchView.clearResults();
+
+        await state.search.getResults();
+
+        searchView.renderResults(state.search.result);
+    }
+
+
+}
+
+elements.searchView.addEventListener('submit', e => {
+    e.preventDefault();
+    controlsearch();
+
+})
+
---
+++ src/js/views/base.js
@@ -0,0 +1,6 @@
+export const elements = {
+ searchInput: document.querySelector('.search__field'),
+ searchView: document.querySelector('.search'),
+ searchResList: document.querySelector('.results__list')
+
+};
